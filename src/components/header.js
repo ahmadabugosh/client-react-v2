54a47750e937e19a4331523e72e7e256b5fc7e24
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-06-27 15:57:06 +0400

    Added state to header

--- src/components/header.js
+++ src/components/header.js
@@ -2,14 +2,40 @@
 
 import React, {Component} from 'react';
 
+import {connect} from 'react-redux';
+import {Link} from 'react-router';
+
 class Header extends Component  {
+
+	renderLinks() {
+		if(this.props.authenticated){
+
+return <li className="nav-item">
+<Link to="/signout" className="nav-link"> Sign Out </Link>
+</li>
+		} else {
+			return [
+<li className="nav-item">
+<Link to="/signin" className="nav-link" key={1}>Sign in</Link>
+</li>,
+<li className="nav-item">
+<Link to="/signup" className="nav-link" key={2}>Sign Up</Link>
+</li>
+];
+
+		}
+		
+
+
+	}
 
 	render() {
 		return (
 			<nav className="navbar navbar-light">
+			<Link to="/" className="navbar-brand"> Redux Auth </Link>
 			<ul className="nav navbar-nav">
 			<li className="nav-item">
-			Sign in
+			{this.renderLinks()}
 			</li>
 			</ul>
 			</nav>
@@ -20,4 +46,13 @@ class Header extends Component  {
 
 }
 
-export default Header;
+function mapStateToProps(state) 
+{
+	return {
+		authenticated: state.auth.authenticated
+
+	};
+
+}
+
+export default connect()(Header);
