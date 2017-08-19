5cb2dc874cf7f4f67877e511388679b9f58f51ab
Jay Ullman <jayullman@gmail.com>
2017-08-19 00:25:25 -0700

    Update component to use username route param

--- src/components/user_profile_public.js
+++ src/components/user_profile_public.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions/';
 import Impact from './impact';
+import { RingLoader } from 'react-spinners';
 
 // component will first check redux store for cached results
 // if the store does not contain an entry for the user, an api call
@@ -14,65 +15,66 @@ class UserProfilePublic extends Component {
   }
   componentDidMount() {
     if (!this._findUserByName()) {
-      this.props.fetchUserInfo(this.props.match.params.user);
+      this.props.fetchUserInfo(this.props.match.params.username);
     }
   }
 
   handleFollowUser(username) {
     // TODO: action creator for following current user
-    console.log(`following ${this.props.match.params.user}`);
+    console.log(`following ${this.props.match.params.username}`);
   }
   _findUserByName() {
     return Object.values(this.props.users).find(
-      user => user.name.toLowerCase() === this.props.match.params.user.toLowerCase()
+      user => user.username.toLowerCase() === this.props.match.params.username.toLowerCase()
     );
   }
 
   render() {
     const user = this._findUserByName();
-    console.log(user);
+    if (this.props.isLoading) {
+      return <RingLoader color={'#123abc'} loading={this.props.isLoading} />;
+    }
+    if (!user) {
+      return <div>User not found</div>;
+    }
     return (
       <div>
-        {!user
-          ? <div>loading</div>
-          : <div>
-              <div>
-                <h3>
-                  {user.name}&#8217;s Stats
-                </h3>
-                <button onClick={this.handleFollowUser} className="btn btn-primary">
-                  Follow {user.name}
-                </button>
-                {user.joinDate &&
-                  <h4>
-                    Join date: {user.joinDate}
-                  </h4>}
-                {user.country &&
-                  <h4>
-                    Country: {user.country}
-                  </h4>}
-                {user.badges &&
-                  <h4>
-                    Badges: {user.badges}
-                  </h4>}
-                {user.organizations &&
-                  <h4>
-                    Organizations: {user.organizations}
-                  </h4>}
-                {user.activities &&
-                  <h4>
-                    Activities: {user.activities}
-                  </h4>}
-              </div>
-              <Impact user={user} />
-            </div>}
+        <div>
+          <h3>
+            {user.name}&#8217;s Stats
+          </h3>
+          <button onClick={this.handleFollowUser} className="btn btn-primary">
+            Follow {user.name}
+          </button>
+          {user.joinDate &&
+            <h4>
+              Join date: {user.joinDate}
+            </h4>}
+          {user.country &&
+            <h4>
+              Country: {user.country}
+            </h4>}
+          {user.badges &&
+            <h4>
+              Badges: {user.badges}
+            </h4>}
+          {user.organizations &&
+            <h4>
+              Organizations: {user.organizations}
+            </h4>}
+          {user.activities &&
+            <h4>
+              Activities: {user.activities}
+            </h4>}
+        </div>
+        <Impact user={user} />
       </div>
     );
   }
 }
 
 const mapStateToProps = state => {
-  return { users: state.users };
+  return { users: state.users, isLoading: state.isLoading };
 };
 
 export default connect(mapStateToProps, actions)(UserProfilePublic);
