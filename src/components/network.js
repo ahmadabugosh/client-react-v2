080067d813be10efe2b66e3a5bef45c2787bc18c
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-10-23 00:31:08 +0400

    Added basic styling semantic ui to network page

--- src/components/network.js
+++ src/components/network.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions/';
 import { RingLoader } from 'react-spinners';
-import {Icon, Button} from 'semantic-ui-react';
+import {Icon, Button,Image, List} from 'semantic-ui-react';
 import axios from 'axios';
 
 const ROOT_URL = 'https://i7san-api.herokuapp.com';
+
+const sizes = ['massive']
+
 
 class Network extends Component {
 
@@ -37,26 +40,45 @@ class Network extends Component {
       <div className="container-fluid">
 
     <div className="row">
+    <img src= "https://s3.amazonaws.com/i7san-test/svg/impact.svg" alt="Volunteering" id="connectImage"  />
         <h1>
           Connect with members of the community to grow your network!
         </h1>
         </div>
+  <br />
+<div className="row">
 
-
-           <div className="row">
-          <img src= "https://s3.amazonaws.com/i7san-test/svg/impact.svg" alt="Volunteering" id="impactImage"  />
-          <div style={tempDivStyle}>
-        
-             
-          </div>
-
-        </div>
-
-          
+       
+   
    
 
-            
+         {sizes.map(size => (
+        <div key={size}>
+          <List divided horizontal size={size}>
+            <List.Item>
+              <Image avatar src='https://react.semantic-ui.com/assets/images/avatar/small/helen.jpg' />
+              <List.Content>
+                <List.Header>Helen</List.Header>
+              </List.Content>
+            </List.Item>
+            <List.Item>
+              <Image avatar src='https://react.semantic-ui.com/assets/images/avatar/small/christian.jpg' />
+              <List.Content>
+                <List.Header>Christian</List.Header>
+              </List.Content>
+            </List.Item>
+            <List.Item>
+              <Image avatar src='https://react.semantic-ui.com/assets/images/avatar/small/daniel.jpg' />
+              <List.Content>
+                <List.Header>Daniel</List.Header>
+              </List.Content>
+            </List.Item>
+          </List>
 
+          <br />
+        </div>
+      ))}   
+   </div>
       
       </div>
     );
